63b232e8fc4bfcccd71c97c37ff11b3448ef3abf
eastmoutain <east.moutain.yang@gmail.com>
2017-11-21 23:24:18 +0800

    this version boot successfully to main

--- kernel/include/boot/descriptor.h
+++ kernel/include/boot/descriptor.h
@@ -1,16 +1,25 @@
 #ifndef DESCRIPTOR_H
 #define DESCRIPTOR_H
 
-#define NULL_SELECTOR 0
-#define KERNEL_CODE32_SELECTOR    0x08
-#define KERNEL_DATA32_SELECTOR    0x10
-#define USER_CODE32_SELECTOR      0x18
-#define USER_DATA32_SELECTOR      0x20
-#define KERNEL_CODE64_SELECTOR    0x28
-#define KERNEL_DATA64_SELECTOR    0x30
-#define USER_CODE64_SELECTOR      0X38
-#define USER_DATA64_SELECTOR      0x40
-#define TSS_SELECTOR              0x48
+
+/*
+ *  * System Selectors
+ *   */
+#define NULL_SELECTOR       0x00
+
+/********* x86 selectors *********/
+#define CODE_SELECTOR       0x08
+#define DATA_SELECTOR       0x10
+#define USER_CODE_32_SELECTOR   0x18
+#define USER_DATA_32_SELECTOR   0x20
+
+/******* x86-64 selectors ********/
+#define CODE_64_SELECTOR    0x28
+#define STACK_64_SELECTOR   0x30
+#define USER_CODE_64_SELECTOR   0x38
+#define USER_DATA_64_SELECTOR   0x40
+
+#define TSS_SELECTOR        0x48
 
 #endif // DESCRIPTOR_H
 
